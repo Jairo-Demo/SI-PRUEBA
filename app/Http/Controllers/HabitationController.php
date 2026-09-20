5c5b0185aa6721ec59833023d27426e68091f09e
Jairo-Demo <47896jhonpro@gmail.com>
2026-09-19 22:26:04 -0400

    Se agrego las migraciones,controles,modells

---
+++ app/Http/Controllers/HabitationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Habitation;
+use Illuminate\Http\Request;
+
+class HabitationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Habitation $habitation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Habitation $habitation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Habitation $habitation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Habitation $habitation)
+    {
+        //
+    }
+}
